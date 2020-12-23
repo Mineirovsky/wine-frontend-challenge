49379a08b00c177999e7b022b8d1677ca1579289
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-23 10:14:52 -0300

    Place Navbar in Header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <div id="app">
     <div id="header">
-
+      <Navbar/>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Navbar from './components/Navbar.vue';
 
+@Component({
+  components: {
+    Navbar,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss">
 @import './assets/styles/constants';
