4b2aa3a8b493c7b69e8078d3e62a2cf3549eec95
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-25 19:27:37 -0300

    Create Banner Component
    It handles responsive images for the banner.

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+  <picture class="banner">
+    <source media="(min-width: 700px)" :srcset="bannerWideImg">
+    <img :src="bannerBoxImg" :alt="altText">
+  </picture>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import bannerWideImg from '@/assets/images/banner-wide.png';
+import bannerBoxImg from '@/assets/images/banner-box.png';
+
+@Component
+export default class Banner extends Vue {
+  bannerWideImg = bannerWideImg
+
+  bannerBoxImg = bannerBoxImg
+
+  altText = 'Big Sale. É big, é desconto, é a Wine caprichando mais uma vez. Vinhos a partir de R$29,90 para sócio Wine. Confira a seleção.'
+}
+</script>
+
+<style lang="scss">
+.banner {
+  margin: 0;
+
+  img {
+    width: 100%;
+  }
+}
+</style>
