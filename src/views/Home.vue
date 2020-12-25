544a6bf9923468676778a2afb170959d823d6fe5
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-25 19:27:51 -0300

    Add Banner Section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <section>
+      <a href="#">
+        <Banner/>
+      </a>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Banner from '@/components/Banner.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    Banner,
   },
 })
 export default class Home extends Vue {}
