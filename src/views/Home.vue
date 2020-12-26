8029812c76f168b65bf6f71b8d65e14e36a0203d
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-26 10:18:47 -0300

    Add Product List to Home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,16 +5,21 @@
         <Banner/>
       </a>
     </section>
+    <section class="home-offers">
+      <ProductList/>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
+import ProductList from '@/components/ProductList.vue';
 
 @Component({
   components: {
     Banner,
+    ProductList,
   },
 })
 export default class Home extends Vue {}
