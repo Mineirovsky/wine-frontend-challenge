7ab0c0a340dea6bef4627ada7e3d47144a4e9672
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-26 09:45:05 -0300

    Create Product Item Interface

---
+++ src/types/ProductItem.ts
@@ -0,0 +1,11 @@
+/**
+ * Product data
+ */
+export default interface ProductItem {
+  available: boolean;
+  image: string;
+  name: string;
+  priceMember: number;
+  pricePromotional: number;
+  priceStock: number;
+}
