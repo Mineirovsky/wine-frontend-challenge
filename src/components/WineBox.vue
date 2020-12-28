7749ab00ed02e220274a4a4f247b28a2352cb0d8
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-28 11:01:33 -0300

    Create Wine Box Component
    Only base structure for initial adjustments.

---
+++ src/components/WineBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wine-box-container">
+    <section class="wine-box">
+
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class WineBox extends Vue {}
+</script>
+
+<style lang="scss">
+@import '@/assets/styles/constants';
+@import '@/assets/styles/colors';
+
+.wine-box-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: rgba(0, 0, 0, .25);
+
+  .wine-box {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    max-width: $base-length * 22;
+    background: $white;
+  }
+}
+</style>
