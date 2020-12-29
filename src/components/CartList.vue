3f85217d4afdc777399490ebd661770e8978d282
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-29 19:11:23 -0300

    Add Border To Cart List Items

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -21,8 +21,26 @@ export default class CartList extends Vue {
 </script>
 
 <style lang="scss">
+@import '@/assets/styles/constants';
+@import '@/assets/styles/colors';
+
 .cart-list {
   padding: 0;
   list-style: none;
+  margin: 0;
+
+  li {
+    padding: $base-length ($base-length * 1.125) $base-length 0;
+
+    &:not(:last-of-type)::after {
+      border-bottom: 1px solid $light-grey;
+      display: block;
+      width: calc(100% - $base-length);
+      content: ' ';
+      margin: 0 0 0 $base-length;
+      position: relative;
+      top: $base-length;
+    }
+  }
 }
 </style>
