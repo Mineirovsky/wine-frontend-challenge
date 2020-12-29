4cd07ec5ee73612303cbee9359723eaba4ca3a17
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2020-12-29 14:32:14 -0300

    Create Cart List Component

---
+++ src/components/CartList.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul class="cart-list">
+    <li>
+      <CartItem/>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import CartItem from './CartItem.vue';
+
+@Component({
+  components: {
+    CartItem,
+  },
+})
+export default class CartList extends Vue {
+
+}
+</script>
+
+<style lang="scss">
+.cart-list {
+  padding: 0;
+  list-style: none;
+}
+</style>
