566defb2e8c83ef4a52bd0d1b3585c59497f4f48
Gabriel Mineiro <gabrielpfgmineiro@gmail.com>
2021-02-18 17:16:20 -0300

    Implement Spinner for Products Loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,14 @@
       </a>
     </section>
     <section class="home-section">
-      <ProductList :items="products" />
+      <Spinner v-if="productsLoading" color="#5CB76F" size="72px" />
+      <ProductList v-else :items="products" />
     </section>
   </div>
 </template>
 
 <script lang="ts">
+import Spinner from 'vue-spinner/src/ClipLoader.vue';
 import { Component, Vue } from 'vue-property-decorator';
 import Banner from '@/components/Banner.vue';
 import ProductList from '@/components/ProductList.vue';
@@ -22,12 +24,18 @@ import Product from '@/types/Product';
   components: {
     Banner,
     ProductList,
+    Spinner,
   },
-  computed: mapState('Products', ['products']),
+  computed: mapState('Products', {
+    products: 'products',
+    productsLoading: 'loading',
+  }),
   methods: mapActions('Products', ['fetchProducts']),
 })
 export default class Home extends Vue {
   products!: Product[]
+
+  productsLoading!: boolean
 
   fetchProducts!: () => Promise<Product[]>
 
